7523cc26b4813a537e466712c48d5cff61d0fb4a
Adil hussain <101266575+Adilhussain34@users.noreply.github.com>
2023-01-23 23:33:49 +0530

    user-login-auntication

---
+++ models/database.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+mongoose
+    .connect(
+        "mongodb://127.0.0.1:27017/passport"
+    )
+    .then(function () {
+        console.log("Database connected!");
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
